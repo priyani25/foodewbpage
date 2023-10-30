72a93090db9b3b79baf9941ad5f01f06e8850807
priyani25 <138353487+priyani25@users.noreply.github.com>
2023-10-30 22:25:24 +0530

    Create insertionsort.cpp

---
+++ insertionsort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original Array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    insertionSort(arr, n);
+
+    std::cout << "\nSorted Array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
